897b1e3c127df1ac11621742e8176769c72057ba
Turing0x <rauligf97@gmail.com>
2023-07-06 17:10:19 -0400

    comienza el trabajo en el dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { LoginService } from './login/login.services';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    routing
   ],
-  providers: [],
+  providers: [LoginService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { LoginService } from './login.services';
+import swal from 'sweetalert2';
+
+import { Clinic } from 'src/data/clinic/clinic';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,6 +12,46 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  clinic: Clinic = new Clinic()  
   whatSee: boolean = true;
+
+  constructor(
+    private loginService: LoginService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ){}
+
+  public makeLogin(): void{
+    this.loginService.signIn(this.clinic).subscribe(
+      success => {
+
+        if( !success ){
+          swal.fire( 'Login error', 
+            'The data provided is incorrect, please rectify it', 'error' )
+          return
+        }
+
+        swal.fire( 'Action completed!', 
+        `You have successfully logged in!`, 'success')
+        this.router.navigate(['/dashboard'], 
+          { relativeTo: this.activatedRoute })
+
+      }
+    )
+  }
+
+  public registerClinic(): void{
+    this.loginService.registerClinic(this.clinic).subscribe(
+      cli => {
+        this.whatSee = !this.whatSee
+        this.clinic = cli
+        swal.fire(
+          'Action completed!',
+          `The clinic has been successfully registered!`,
+          'success'
+        )
+      }
+    )
+  }
 
 }
