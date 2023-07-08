08027b90378d8ab9f5afc1f7612264953c0684fc
Turing0x <83433135+Turing0x@users.noreply.github.com>
2023-07-07 22:43:16 -0400

    Revert "feature: arreglando salsitas"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,40 +1,22 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {BodySiteComponent} from "./shared/body-site/body-site.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: BodySiteComponent,
-    children: [
-      {
-        path: 'auth',
-        loadChildren: () =>
-          import('./modules/auth/auth.module')
-            .then(m => m.AuthModule)
-      },
-      {
-        path: 'dashboard',
-        loadChildren: () =>
-          import('./modules/dashboard/dashboard.module')
-            .then(m => m.DashboardModule)
-      },
-      {
-        path: '', pathMatch: "full", redirectTo: 'auth'
-      }
-    ]
+  { 
+    path: 'auth',
+    loadChildren: () => 
+      import('./auth/auth.module')
+        .then( m => m.AuthModule )
   },
-
-  //esto debjo se llama will cards si lo usas utilizalo para mandar a una pagina de not found o algo asi par que
-  // la gente no escriba mierda en la ruta, que si te escribe cualquier cosa que no tienes ene l router definidio
-  // te lo envie para esa pagina ejemplo
-  // {
-  //     path: '**', redirectTo: 'not-found'
-  //  },
-  // y entonces tengas una ruta not -found para ti
-  // {
-  //   path: '**', redirectTo: 'auth'
-  // }
+  {
+    path: 'dashboard', 
+    loadChildren: () => 
+      import('./dashboard/dashboard.module')
+        .then( m => m.DashboardModule )
+  },
+  {
+    path: '**', redirectTo: 'auth'
+  }
 ]
 
 @NgModule({
@@ -42,5 +24,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 
-export class AppRoutingModule {
-}
+export class AppRoutingModule { }
--- src/app/body-site/body-site.component.ts
+++ src/app/body-site/body-site.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'body-site-main',
   templateUrl: './body-site.component.html',
-  styleUrls: ['./body-site.component.scss']
+  styleUrls: ['./body-site.component.css']
 })
 export class BodySiteComponent {
 
